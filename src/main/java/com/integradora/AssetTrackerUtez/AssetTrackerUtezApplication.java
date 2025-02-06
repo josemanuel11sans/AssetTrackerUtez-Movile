80f932e5f1c7c3981dd2747e60d9e5d5b125d4c3
josemanuel11sans <20233tn078@utez.edu.mx>
2025-02-05 23:41:21 -0600

    se crea el repo

---
+++ src/main/java/com/integradora/AssetTrackerUtez/AssetTrackerUtezApplication.java
@@ -0,0 +1,13 @@
+package com.integradora.AssetTrackerUtez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetTrackerUtezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetTrackerUtezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/integradora/AssetTrackerUtez/AssetTrackerUtezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integradora.AssetTrackerUtez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetTrackerUtezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
